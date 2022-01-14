db7cc6e48e4f73ddee9e2b5f10a845b13b84eff3
Hudson <hs66417@ohio.edu>
2022-01-14 15:14:30 -0500

    Success

--- filesize.cc
+++ filesize.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 // output format to use IDENTICALLY:
@@ -11,8 +12,40 @@ using namespace std;
 int main( int argc, char* argv[] )
 {
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
+    /*for (int arg = 0; arg < argc; ++arg){
             std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+    }*/
+    fstream input;
+    string line;
+    int lines=0;
+    cout << "program: " << argv[0] << endl;
+    for(int arg = 1; arg < argc; arg++)
+    {
+        lines = 0;
+        input.open(argv[arg]);
+        //cout << "opened file " << argv[arg] << endl;
+        if(!input.is_open())
+        {
+            cout << " " << argv[arg] << ": " << "-1" << endl;
+        }
+        else
+        {
+            while(!input.eof())
+            {
+                getline(input, line);
+                //if(line.length()>0)
+                {
+                    lines++;
+                    //cout << "Lines = " << lines << endl;
+                }
+            }
+            cout << " " << argv[arg] << ": " << lines << endl;
+        }
+        input.close();
+    }
+
+
+    
 
     exit(0); // this means that the program executed correctly!
 }
